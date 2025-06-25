db48a8affe3a4264c8c1d2fae040f96d2bcffb84
Eng-HanifLeghari <m.haneef1966@gmail.com>
2025-06-25 13:12:25 +0500

    today my first commit

--- downloader.1/downloader/urls.py
+++ downloader.1/downloader/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     path('api/download/', views.index, name='index'),  # Home page for the downloader form
-                  path('', views.start_download, name='start_download'),
-                  path('api/download/status/<str:task_id>/', views.check_status, name='check_status'),
-                  path('api/download/file/<str:task_id>/', views.download_file, name='download_file'),
-              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('', views.start_download, name='start_download'),
+    path('api/download/status/<str:task_id>/', views.check_status, name='check_status'),
+    path('api/download/file/<str:task_id>/', views.download_file, name='download_file'),
+    ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
